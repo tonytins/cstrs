b997d03621f91638d98f3228ec8a95862e91ad58
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-07-21 18:23:31 -0400

    Got UIText working in a day!

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use substring::Substring;
+use walkdir::WalkDir;
 
 #[cfg(windows)]
 const LINE_ENDING: &'static str = "\r\n";
 #[cfg(not(windows))]
 const LINE_ENDING: &'static str = "\n";
+
+const MISSING: &'static str = "***MISSING***";
 
 const CARET: char = '^';
 
@@ -44,21 +51,69 @@ pub fn get_entry<S: Into<String>>(content: S, key: usize) -> String {
     }
 
     // No entry found.
-    return "***MISSING***".to_string();
+    return MISSING.to_string();
 }
 
-pub struct UIText {}
+pub struct UIText {
+    pub language: String,
+}
 
-impl UIText {}
+impl UIText {
+    pub fn new<S: Into<String>>(language: S) -> UIText {
+        UIText {
+            language: language.into(),
+        }
+    }
+
+    pub fn get_text(&self, id: usize, key: usize) -> String {
+        let language_dir = format!(
+            "{}/uitext/{}.dir/",
+            env::current_dir().unwrap().display(),
+            self.language
+        );
+
+        for entry in WalkDir::new(&language_dir)
+            .sort_by_file_name()
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
+            let f_name = entry.file_name().to_string_lossy();
+            dbg!(&f_name);
+            if f_name.ends_with(".cst") {
+                let mut open_file =
+                    match File::open(Path::new(format!("{}/{}", language_dir, f_name).as_str())) {
+                        Err(_) => panic!("couldn't open {}", f_name),
+                        Ok(file) => file,
+                    };
+                let mut contents = String::new();
+                match open_file.read_to_string(&mut contents) {
+                    Ok(_) => return get_entry(contents, key),
+                    Err(why) => panic!("couldn't read {}: {}", f_name, why),
+                }
+            }
+        }
+
+        dbg!(language_dir);
+        return MISSING.to_string();
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_entry, normalize_entries};
+    use crate::{get_entry, normalize_entries, UIText};
 
     #[cfg(windows)]
     const LINE_ENDING: &'static str = "\r\n";
     #[cfg(not(windows))]
     const LINE_ENDING: &'static str = "\n";
+
+    #[test]
+    fn get_uitext() {
+        let ui_text = UIText::new("lorem");
+        let expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum orci, at tincidunt nulla. Donec aliquet, eros non interdum posuere, ipsum sapien molestie nunc, nec facilisis libero ipsum et risus. In sed lorem vel ipsum placerat viverra.".to_string();
+        dbg!(ui_text.get_text(101, 1));
+        assert_eq!(ui_text.get_text(101, 1), expected);
+    }
 
     #[test]
     fn is_normalized() {
@@ -67,7 +122,6 @@ mod tests {
             LINE_ENDING, LINE_ENDING
         );
         let input = normalize_entries(example);
-        dbg!(format!("{:?}", input));
         let expected = [
             "1 ^The quick brown fox".to_string(),
             "2 ^jumps over the lazy dog".to_string(),
